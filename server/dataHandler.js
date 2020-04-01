a1d216f2e6b919d96fb881ef7dc4fb5f7aadf76c
Robin Lack <lack@puzzle.ch>
2020-04-01 10:27:23 +0200

    parse Altersverteilung sheet

--- server/dataHandler.js
+++ server/dataHandler.js
@@ -9,7 +9,6 @@ function handleEpiKurve(filename, sheetname) {
                 if(i>3){
                     if(j==0){
                         dataObject.date=rows[i][j];
-                        
                     }
                     else{
                         dataObject.cases=rows[i][j];
@@ -21,12 +20,58 @@ function handleEpiKurve(filename, sheetname) {
         console.log(data);
     })
 }
+
 function handleAltersVerteilung(filename, sheetname) {
-    xlsxFile(`./files/${filename}`,{ sheet: sheetname }).then((rows) => {for (i in rows){
-        for (j in rows[i]){
-            console.log(rows[i][j]);
+    xlsxFile(`./files/${filename}`,{ sheet: sheetname }).then((rows) => {
+        let data = [];
+        let dataObject = {};
+        for (i in rows){
+            dataObject = new Object();
+            for (j in rows[i]){
+                if(i>=5 && i<=13){
+                    switch (j) {
+                        case "0":
+                            dataObject.age=rows[i][j];
+                        break;
+                        case "1":
+                            dataObject.maleInfectionCount=rows[i][j];
+                        break;
+
+                        case "2":
+                            dataObject.malePercentage=rows[i][j];
+                        break;
+
+                        case "3":
+                            dataObject.maleIncidence=rows[i][j];
+                        break;
+
+                        case "4":
+                            dataObject.femaleInfectionCount=rows[i][j];
+                        break;
+
+                        case "5":
+                            dataObject.femalePercentage=rows[i][j];
+                        break;
+
+                        case "6":
+                            dataObject.femaleIcidence=rows[i][j];
+                        break;
+                        
+                        case "7":
+                            dataObject.totalInfectionCount=rows[i][j];
+                        break;
+                        
+                        case "8":
+                            dataObject.totalPercentage=rows[i][j];
+                            data.push(dataObject);
+                        break;
+
+                    }
+                }
+            }
         }
-    }})
+        console.log(data);
+    })
 }
 function handleKantone(filename, sheetname) {
     // xlsxFile(`./files/${filename}`,{ sheet: sheetname }).then((rows) => {for (i in rows){
