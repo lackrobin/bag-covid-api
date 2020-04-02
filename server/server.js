ed88a1d0a923b60a5f0e20eac1133ac9a2868757
Robin Lack <lack@puzzle.ch>
2020-04-02 19:04:37 +0200

    updated endpoint

--- server/server.js
+++ server/server.js
@@ -20,18 +20,38 @@ app.use(function(req, res, next) {
     next();
   });
   
-  app.get("/api", (req, res) =>
-  res.send(JSON.stringify({
+  app.get("/api", (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({
     api: [
       {
         endpoint: "/data",
         requestType: "get",
-        description: "get all data"
+        description: "get all date values",
+        "/": [
+          {
+            endpoint : "{YYYY-MM-DD}",
+            requestType : "get",
+            description : "get official data from specific date",
+            example: "/data/2020-04-02"
+          }
+        ]
       }
     ]
   }))
-  );
+});
   
+  app.get("/api/data/", (req, res) => {
+    let obj = {dates: []};
+    db.createKeyStream()
+  .on('data', function (data) {
+    console.log(data);
+    obj.dates.push(data);
+  }).on('end', function(){
+    res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(obj));
+  });
+});
   
   app.get("/api/data/:date", (req, res) => {
         db.get(req.params.date, function(err, value) {
