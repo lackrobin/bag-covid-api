20f426227b5ea320bfddbf33b1488514ff09a02e
Robin Lack <lack@puzzle.ch>
2020-04-04 12:12:44 +0200

    added endpoint for latest dataset

--- server/server.js
+++ server/server.js
@@ -33,13 +33,19 @@ app.use(function(req, res, next) {
       {
         endpoint: "/data",
         requestType: "get",
-        description: "get all date values",
+        description: "get all available date values",
         "/": [
           {
             endpoint : "{YYYY-MM-DD}",
             requestType : "get",
             description : "get official data from specific date",
             example: "/data/2020-04-02"
+          },
+          {
+            endpoint : "latest",
+            requestType : "get",
+            description : "get official data from latest date available",
+            example: "/data/latest"
           }
         ]
       }
@@ -47,16 +53,39 @@ app.use(function(req, res, next) {
   }))
 });
   
-  app.get("/api/data/", (req, res) => {
-    let obj = {dates: []};
-    db.createKeyStream()
-  .on('data', function (data) {
-    console.log(data);
-    obj.dates.push(data);
-  }).on('end', function(){
-    res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(obj));
+app.get("/api/data/", (req, res) => {
+  let obj = {dates: []};
+  db.createKeyStream()
+.on('data', function (data) {
+  console.log(data);
+  obj.dates.push(data);
+}).on('end', function(){
+  res.setHeader('Content-Type', 'application/json');
+res.end(JSON.stringify(obj));
+});
+});
+
+app.get("/api/data/latest", (req, res) => {
+  let latestDate;
+  db.createKeyStream()
+.on('data', function (data) {
+  if(!latestDate){
+    latestDate=data;
+  }
+  else{
+    latestDate = Date.parse(data) > Date.parse(latestDate) ? data : latestDate;
+  }
+}).on('end', function(){
+  db.get(latestDate, function(err, value) {
+    if (err) {
+      console.log(err);
+      res.send({err});
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(value);
+    }
   });
+});
 });
   
   app.get("/api/data/:date", (req, res) => {
