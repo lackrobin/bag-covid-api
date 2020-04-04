188e8b29b26d6623f582f838fff1f53049956a73
Robin Lack <lack@puzzle.ch>
2020-04-04 19:53:40 +0200

    allow cross origin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || portSwitch;
 
 app.use(express.json());
 app.use(function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "http://localhost:1234"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
