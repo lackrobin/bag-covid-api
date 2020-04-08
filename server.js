82c21a98e969240b11284a12ad7b036e62bbe37d
Robin Lack <lack@puzzle.ch>
2020-04-08 21:19:55 +0200

    upgraded api, improved handling of data and files

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const { downloadAndParse } = require("./util/downloadAndParse");
+const { downloadAndParse, parseAllFiles,copyFilesToFolder } = require("./util/downloadAndParse");
 const express = require("express");
 const { db } = require("./util/db");
 const  schedule =  require('node-schedule');
 
-downloadAndParse();
-schedule.scheduleJob('0 18 * * *', downloadAndParse); 
+const dirname = "files/";
 
 const app = express();
 let portSwitch = 3000;
@@ -37,18 +36,39 @@ app.use(function(req, res, next) {
         description: "get all available date values",
         "/": [
           {
-            endpoint : "{YYYY-MM-DD}",
+            endpoint : "/{YYYY-MM-DD}",
             requestType : "get",
             description : "get official data from specific date",
             example: "/data/2020-04-02"
           },
           {
-            endpoint : "latest",
+            endpoint : "/latest",
             requestType : "get",
             description : "get official data from latest date available",
             example: "/data/latest"
           }
         ]
+      }
+      ,
+      {
+        endpoint : "/hospit",
+        requestType : "get",
+        description : "get TOTAL number of hospitalized patients for age groups as of date",
+        example: "/data/latest"
+      }
+      ,
+      {
+        endpoint : "/death",
+        requestType : "get",
+        description : "get TOTAL number of deaths for age groups as of date",
+        example: "/data/latest"
+      }
+      ,
+      {
+        endpoint : "/infection",
+        requestType : "get",
+        description : "get TOTAL number of infected patients for age groups as of date",
+        example: "/data/latest"
       }
     ]
   }))
@@ -89,17 +109,63 @@ app.get("/api/data/latest", (req, res) => {
 });
 });
   
-  app.get("/api/data/:date", (req, res) => {
-        db.get(req.params.date, function(err, value) {
-          if (err) {
-            console.log(err);
-            res.send({err});
-          } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(value);
-          }
-        });
-    });
+app.get("/api/data/:date", (req, res) => {
+      db.get(req.params.date, function(err, value) {
+        if (err) {
+          console.log(err);
+          res.send({err});
+        } else {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(value);
+        }
+      });
+});
 
-  
-  app.listen(port, () => console.log(`BAG COVID API listening on port ${port}!`));
+app.get("/api/hospit", (req, res) => {
+  const subsetName = "COVID19 Altersverteilung Hospit";
+  const valueName = "TotalHospitalized";
+  getSingleElementFromAllDataSets(res, subsetName, valueName);
+});
+
+app.get("/api/death", (req, res) => {
+  const subsetName = "COVID19 Altersverteilung TodF";
+  const valueName = "TotalDeaths";
+  getSingleElementFromAllDataSets(res, subsetName, valueName);
+});
+
+app.get("/api/infection", (req, res) => {
+  const subsetName = "COVID19 Altersverteilung";
+  const valueName = "totalInfectionCount";
+  getSingleElementFromAllDataSets(res, subsetName, valueName);
+});
+
+app.listen(port, () => console.log(`BAG COVID API listening on port ${port}!`));
+
+function getSingleElementFromAllDataSets(res, subsetName, valueName) {
+  let responseData = [];
+  let dataObject = {};
+  db.createKeyStream().on('data', function (key) {
+    db.get(key, function (err, data) {
+      dataObject = { date: key };
+      if (err) {
+        console.log(err);
+        res.send({ err });
+      }
+      else {
+        data = JSON.parse(data);
+        data = data.data[subsetName];
+        data.forEach(element => {
+        dataObject[element.age] = element[valueName];
+        });
+        responseData.push(dataObject);
+      }
+    });
+  }).on('close', function () {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(responseData));
+  });
+}
+
+copyFilesToFolder("filesarchive/",dirname, function(){parseAllFiles(dirname)});
+parseAllFiles(dirname);
+schedule.scheduleJob('0 18 * * *', downloadAndParse); 
--- util/downloadAndParse.js
+++ util/downloadAndParse.js
@@ -15,66 +15,103 @@ function downloadAndParse() {
   month = month.toString.length = 1 ? "0" + month : "" + month;
   let day = date.getDate();
   day = day.toString.length = 1 ? "0" + day : "" + day;
-  const dest = `files/${date.getFullYear()}-${month}-${day}.xlsx`;
+  const filename = `${date.getFullYear()}-${month}-${day}.xlsx;`
+  const dirname = "files/";
+  const dest = `${dirname}${filename}`;
   console.log("starting download...");
-
   download(url, dest, function (err) {
     if (err) {
       console.log(err);
     }
     else {
         console.log("download complete!");
-        console.log("read files...");
-      fs.readdir("files/", (err, filenames) => {
-        if (err) {
-          console.log(err);
-        }
-        filenames.forEach(filename => {
-            if (filename !== ".gitignore") {
-            console.log("processing file: "+ filename);
-            let filenameDate = filename.substr(0, 10);
-            let data = {};
-            let workbook = xlsx.readFile("files/" + filename);
-            workbook.SheetNames.forEach(sheetName => {
-              let worksheet = workbook.Sheets[sheetName];
-              let sheetData = {};
-              switch (sheetName) {
-                case "COVID19 Epikurve":
-                  sheetData = handleEpiKurve(sheet2arr(worksheet));
-                  break;
-                case "COVID19 Altersverteilung":
-                  sheetData = handleAltersVerteilung(sheet2arr(worksheet));
-                  break;
-                case "COVID19 Kantone":
-                  sheetData = handleKantone(sheet2arr(worksheet));
-                  break;
-                case "COVID19 Altersverteilung Hospit":
-                  sheetData = hanldeHospit(sheet2arr(worksheet));
-                  break;
-                case "COVID19 Altersverteilung TodF":
-                  sheetData = handleTod(sheet2arr(worksheet));
-                  break;
-              }
-              data[sheetName] = sheetData;
-            });
-            let fileData = new FileData(filenameDate, data);
-            console.log("processed file: " + filename);
-            console.log("saving data from file: " + filename);
-            saveToDB(fileData);
-          }
-        });
-      });
+        parseFile(filename);
     }
   });
 }
-exports.downloadAndParse = downloadAndParse;
+
+function copyFilesToFolder(srcDir, targetDir, cb){
+  console.log(`copy files from ${srcDir} ro ${targetDir}`);
+  fs.readdir(targetDir, (err, filenames) => {
+    if (err) {
+      console.log(err);
+    }
+    else{
+      if(filenames.length<=1){
+        fs.readdir(srcDir, (err, filenames) => {
+          filenames.forEach((filename) => {fs.copyFile(srcDir+filename,targetDir+filename, (err) => {
+            if (err) console.log(err);
+          });
+        });
+        cb();
+        });
+      }
+    }    
+  })
+    
+  
+}
+
+function parseAllFiles(dirname) {
+  console.log("processing dir: "+ dirname);
+  fs.readdir(dirname, (err, filenames) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      filenames.forEach(parseFile);
+    }
+  });
+}
+
+function parseFile(filename) {
+    if (filename !== ".gitignore") {
+      console.log("processing file: " + filename);
+      let filenameDate = filename.substr(0, 10);
+      let data = {};
+      let workbook = xlsx.readFile("files/" + filename);
+      workbook.SheetNames.forEach(sheetName => {
+        let worksheet = workbook.Sheets[sheetName];
+        let sheetData = {};
+        switch (sheetName) {
+          case "COVID19 Epikurve":
+            sheetData = handleEpiKurve(sheet2arr(worksheet));
+            break;
+          case "COVID19 Altersverteilung":
+            sheetData = handleAltersVerteilung(sheet2arr(worksheet));
+            break;
+          case "COVID19 Kantone":
+            sheetData = handleKantone(sheet2arr(worksheet));
+            break;
+          case "COVID19 Altersverteilung Hospit":
+            sheetData = hanldeHospit(sheet2arr(worksheet));
+            break;
+          case "COVID19 Altersverteilung TodF":
+            sheetData = handleTod(sheet2arr(worksheet));
+            break;
+        }
+        data[sheetName] = sheetData;
+      });
+      let fileData = new FileData(filenameDate, data);
+      console.log("processed file: " + filename);
+      console.log("saving data from file: " + filename);
+      saveToDB(fileData);
+    }
+  }
+
 
 function saveToDB(data) {
   db.put(data.date, JSON.stringify(data), function (err) {
     if (err)
       console.log(err);
     else {
-        console.log("saved to db:" + JSON.stringify(data));
+      console.log("saved to db:" + JSON.stringify(data.date));
     }
   });
+}
+
+module.exports = {
+  downloadAndParse,
+  parseAllFiles,
+  copyFilesToFolder
 }
