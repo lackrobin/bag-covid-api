1b9a465d097756480d95af787ef958896b14e53f
Robin Lack <lack@puzzle.ch>
2020-04-10 20:57:55 +0200

    fixed dates in filename

--- util/downloadAndParse.js
+++ util/downloadAndParse.js
@@ -12,9 +12,9 @@ function downloadAndParse() {
   const url = "https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-datengrundlage-lagebericht.xlsx.download.xlsx/200325_Datengrundlage_Grafiken_COVID-19-Bericht.xlsx";
   let date = new Date();
   let month = date.getMonth() + 1;
-  month = month.toString.length = 1 ? "0" + month : "" + month;
+  month = month.toString().length === 1 ? "0" + month : "" + month;
   let day = date.getDate();
-  day = day.toString.length = 1 ? "0" + day : "" + day;
+  day = day.toString().length === 1 ? "0" + day : "" + day;
   const filename = `${date.getFullYear()}-${month}-${day}.xlsx`;
   const dirname = "files/";
   const dest = `${dirname}${filename}`;
